6c235cd2df4d3e2812120311c9453da32b2c01e6
WillyBoys <ethan.wilhelmsen9969@gmail.com>
2024-01-19 19:09:43 -0700

    Updating

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-vue-app/",
+  base: "/Quishing/",
   plugins: [vue()],
   server: {
     proxy: {
